1273728f6ed8b9b74905c072b0bd8223b70e496e
Julie Yaunches <jmyaunch@gmail.com>
2013-01-15 14:13:03 -0500

    Julie: adding OCMock

---
+++ FindOnTheWayTests/CumulativeRouteStepTest.h
@@ -0,0 +1,13 @@
+//
+//  CumulativeRouteStepTest.h
+//  FindOnTheWay
+//
+//  Created by Julietta Yaunches on 1/15/13.
+//  Copyright (c) 2013 Julietta Yaunches. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CumulativeRouteStepTest : NSObject
+
+@end
---
+++ FindOnTheWayTests/CumulativeRouteStepTest.m
@@ -0,0 +1,15 @@
+//
+//  CumulativeRouteStepTest.m
+//  FindOnTheWay
+//
+//  Created by Julietta Yaunches on 1/15/13.
+//  Copyright (c) 2013 Julietta Yaunches. All rights reserved.
+//
+
+#import <OCMock/OCMock.h>
+#import "CumulativeRouteStepTest.h"
+
+
+@implementation CumulativeRouteStepTest
+
+@end
